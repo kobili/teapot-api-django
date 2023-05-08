d1100d498df9f526c623247c6ea37d11d876191b
Kobe Li <likobe23@gmail.com>
2023-05-08 13:46:53 -0700

    Add delete address endpoint

--- address/views.py
+++ address/views.py
@@ -15,7 +15,7 @@ class AddressViewSet(GenericViewSet):
 
     def get_queryset(self):
         return self.queryset
-    
+
     def create(self, request, user_id=None):
         user = get_user_by_id(user_id)
         if not user:
@@ -30,7 +30,7 @@ class AddressViewSet(GenericViewSet):
             self.serializer_class(address).data,
             status=status.HTTP_201_CREATED,
         )
-    
+
     def retrieve(self, request, user_id=None, pk=None):
         user = get_user_by_id(user_id)
         if not user:
@@ -39,12 +39,12 @@ class AddressViewSet(GenericViewSet):
         address = self._get_user_address_by_id(app_user=user, address_id=pk)
         if not address:
             return self._address_not_found_response(pk)
-        
+
         return Response(
             self.serializer_class(address).data,
             status=status.HTTP_200_OK,
         )
-    
+
     def list(self, request, user_id=None):
         user = get_user_by_id(user_id)
         if not user:
@@ -56,7 +56,7 @@ class AddressViewSet(GenericViewSet):
             self.serializer_class(addresses, many=True).data,
             status=status.HTTP_200_OK,
         )
-    
+
     def update(self, request, user_id=None, pk=None):
         user = get_user_by_id(user_id)
         if not user:
@@ -74,13 +74,26 @@ class AddressViewSet(GenericViewSet):
             serializer.data,
             status=status.HTTP_200_OK,
         )
+
+    def destroy(self, request, user_id=None, pk=None):
+        user = get_user_by_id(user_id)
+        if not user:
+            return user_not_found_response(user_id)
         
+        address = self._get_user_address_by_id(app_user=user, address_id=pk)
+        if not address:
+            return self._address_not_found_response(address_id=pk)
+        
+        address.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
     def _get_user_address_by_id(self, app_user=None, address_id=None):
         try:
             return self.queryset.get(app_user=app_user, address_id=address_id)
         except Address.DoesNotExist:
             return None
-        
+
     def _address_not_found_response(self, address_id=None):
         return Response(
             {"error": f"Could not find address {address_id}"},
