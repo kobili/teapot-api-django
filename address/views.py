28a1f1e2d446da9a4df4e4a0a28d12a89f286946
Kobe Li <likobe23@gmail.com>
2023-05-08 12:59:13 -0700

    Update retrieve endpoint

--- address/views.py
+++ address/views.py
@@ -8,7 +8,7 @@ from .models import Address
 from .serializers import AddressSerializer
 
 # Create your views here.
-class AddressViewSet(GenericViewSet, RetrieveModelMixin):
+class AddressViewSet(GenericViewSet):
     queryset = Address.objects.all()
     serializer_class = AddressSerializer
 
@@ -35,6 +35,28 @@ class AddressViewSet(GenericViewSet, RetrieveModelMixin):
             status=status.HTTP_201_CREATED,
         )
     
+    def retrieve(self, request, user_id=None, pk=None):
+
+        user = self._get_user(user_id=user_id)
+        if not user:
+            return Response(
+                {"error": f"Could not find user {user_id}"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        
+        try:
+            address = self.queryset.get(app_user=user, address_id=pk)
+        except Address.DoesNotExist:
+            return Response(
+                {"error": "Could not find address with id {}".format(pk)},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        
+        return Response(
+            self.serializer_class(address).data,
+            status=status.HTTP_200_OK,
+        )
+    
     def list(self, request, user_id=None):
         UserModel = get_user_model()
 
@@ -51,3 +73,11 @@ class AddressViewSet(GenericViewSet, RetrieveModelMixin):
             self.serializer_class(addresses, many=True).data,
             status=status.HTTP_200_OK,
         )
+    
+    def _get_user(self, user_id=None):
+        UserModel = get_user_model()
+
+        try:
+            return UserModel.objects.get(user_id=user_id, is_active=True)
+        except UserModel.DoesNotExist:
+            return None
