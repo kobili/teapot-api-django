6e3551e7e1d31d607fa2090ea44e68db45deb0cd
Kobe Li <likobe23@gmail.com>
2023-05-08 14:05:53 -0700

    Add addresses to user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
 from .models import AppUser
+from address.serializers import AddressSerializer
 
 class AppUserSerializer(serializers.ModelSerializer):
+    addresses = AddressSerializer(many=True, required=False)
+
     class Meta:
         model = AppUser
-        fields = ['user_id', 'email', 'first_name', 'last_name', 'phone_number', 'is_active', 'is_staff']
+        fields = ['user_id', 'email', 'first_name', 'last_name', 'phone_number', 'is_active', 'is_staff', 'addresses']
 
 class RegistrationSerializer(serializers.Serializer):
     email = serializers.EmailField()
