2a1710322f9b55ac385e7930f5b217b293933842
Kobe Li <likobe23@gmail.com>
2023-05-10 21:34:36 -0700

    Add update payment endpoint

--- payment/views.py
+++ payment/views.py
@@ -29,3 +29,29 @@ class PaymentInfoViewset(GenericViewSet):
             self.serializer_class(payment).data,
             status=status.HTTP_201_CREATED,
         )
+    
+    def update(self, request, user_id=None, pk=None):
+        user = get_user_by_id(user_id)
+        if not user:
+            return user_not_found_response(user_id)
+        
+        serializer = self.serializer_class(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        try:
+            payment = self.queryset.get(app_user=user, payment_id=pk)
+        except PaymentInfo.DoesNotExist:
+            return Response(
+                {"error": f"Could not find payment method {pk}"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        
+        serializer = self.serializer_class(payment, request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK,
+        )
