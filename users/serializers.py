774d2e1fceefd17409a3f8a0fbdcfcf11131aa9b
Kobe Li <likobe23@gmail.com>
2023-05-10 21:08:53 -0700

    Add payment methods to user response

--- users/serializers.py
+++ users/serializers.py
@@ -1,13 +1,25 @@
 from rest_framework import serializers
 from .models import AppUser
 from address.serializers import AddressSerializer
+from payment.serializers import PaymentInfoSerializer
 
 class AppUserSerializer(serializers.ModelSerializer):
     addresses = AddressSerializer(many=True, required=False)
+    payment_methods = PaymentInfoSerializer(many=True, required=False)
 
     class Meta:
         model = AppUser
-        fields = ['user_id', 'email', 'first_name', 'last_name', 'phone_number', 'is_active', 'is_staff', 'addresses']
+        fields = [
+            'user_id',
+            'email',
+            'first_name',
+            'last_name',
+            'phone_number',
+            'is_active',
+            'is_staff',
+            'addresses',
+            'payment_methods',
+        ]
 
 class RegistrationSerializer(serializers.Serializer):
     email = serializers.EmailField()
