1084c55d8e268ee67c7b9c0b6e1cacaf22c4a02c
Kobe Li <likobe23@gmail.com>
2023-05-13 03:35:40 -0700

    Start product app

---
+++ product/models.py
@@ -0,0 +1,17 @@
+from uuid import uuid4
+from django.db import models
+
+from teapot_api import settings
+from category.models import Category
+
+# Create your models here.
+class Product(models.Model):
+    product_id = models.UUIDField(primary_key=True, default=uuid4)
+    name = models.CharField()
+    description = models.CharField()
+    price = models.FloatField()
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="products")
+    category = models.ForeignKey(Category)
