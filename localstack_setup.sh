227ac60d3c92015d6ba6419f419faaa1cbff20bd
Kobe Li <50648660+kobili@users.noreply.github.com>
2023-06-09 18:38:23 -0700

    Update localstack_setup.sh

--- localstack_setup.sh
+++ localstack_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo Setting up S3 Bucket for local stack
 
-awslocal s3api create-bucket --bucket s3-images-kli-99
+awslocal s3api create-bucket --bucket <YOUR_S3_BUCKET_NAME_HERE>
 
 # Define CORS config
 cat << EOF > localstack-s3-cors-config.json
@@ -29,5 +29,5 @@ cat << EOF > localstack-s3-cors-config.json
 }
 EOF
 
-awslocal s3api put-bucket-cors --bucket s3-images-kli-99 --cors-configuration file://localstack-s3-cors-config.json
-awslocal s3api get-bucket-cors --bucket s3-images-kli-99
+awslocal s3api put-bucket-cors --bucket <YOUR_S3_BUCKET_NAME_HERE> --cors-configuration file://localstack-s3-cors-config.json
+awslocal s3api get-bucket-cors --bucket <YOUR_S3_BUCKET_NAME_HERE>
